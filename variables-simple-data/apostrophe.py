f5a927770ee09224dc74f23eb20b084de480396a
urassaya <vanessafoxandrade@hotmail.com>
2024-03-27 15:11:13 +0100

    commit

---
+++ variables-simple-data/apostrophe.py
@@ -0,0 +1,2 @@
+message = "One of Python's strengths is its diverse community." # usar double quotes quando dentro da nossa string usamos single quote
+print(message)
---
+++ variables-simple-data/fullname.py
@@ -0,0 +1,5 @@
+first_name = 'rosamund'
+last_name = 'pike'
+full_name = f'{first_name} {last_name}' # atribuir valores de variáveis a uma variável
+message = f'Hello, {full_name.title()}.'
+print(message)
