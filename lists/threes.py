9e04088b8b8edc794dcfd02a225d3760311e6914
urassaya <vanessafoxandrade@hotmail.com>
2024-03-29 21:49:35 +0100

    Update threes.py

--- lists/threes.py
+++ lists/threes.py
@@ -3,4 +3,5 @@ numbers = list(range(3, 31))
 for number in numbers:
     if number % 3 == 0:
         print(number)
-        
+
+# podia ser apenas numbers = list(range(3, 31, 3)) e depois usar o loop for para printar
