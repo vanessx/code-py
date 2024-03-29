b493b9883389dc0cd34e8696e8206029547ea761
urassaya <vanessafoxandrade@hotmail.com>
2024-03-29 21:57:42 +0100

    Update cubes.py

--- lists/cubes.py
+++ lists/cubes.py
@@ -1,8 +1,10 @@
 cubes = []
 
+# calcula o valor cúbico e adiciona o valor a lista
 for number in range(1, 11):
     cube = number ** 3
     cubes.append(cube)
 
+# printa cada valor que existe na lista
 for cube in cubes:
     print(cube)
