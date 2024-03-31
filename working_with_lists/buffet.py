9a1a147c3831485aa6a1c5c4739b633bad3061f2
urassaya <vanessafoxandrade@hotmail.com>
2024-03-31 18:13:40 +0200

    Create buffet.py

---
+++ working_with_lists/buffet.py
@@ -0,0 +1,13 @@
+# tuple
+portuguese_foods = ('bacalhau com natas', 'arroz de marisco',
+                    'carne de porco alentejana', 'natas do céu', 'arroz de molho pardo')
+
+for food in portuguese_foods:
+    print(food)
+
+# alterar o conteúdo de uma tuple só desta maneira
+portuguese_foods = ('bacalhau com natas', 'feijão preto',
+                    'carne de porco alentejana', 'arroz doce', 'arroz de molho pardo')
+
+for food in portuguese_foods:
+    print(food)
