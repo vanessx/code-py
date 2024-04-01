14d45ddf3c1009c45b0ff097c491478a71738a03
urassaya <vanessafoxandrade@hotmail.com>
2024-04-01 21:21:46 +0200

    commit

---
+++ if/hello_admin.py
@@ -0,0 +1,10 @@
+usernames = ['admin', 'rosamundpike', 'margotrobbie', 'davidst', 'danmar']
+# analisa se a lista está vazia ou não
+if usernames:
+    for user in usernames:
+        if user == 'admin':
+            print(f'Hello {user}, would you like to see a status report?')
+        else:
+            print(f'Hello {user}, thank you for logging in again!')
+else:
+    print('We need to find some users!')
--- if/teste.py
+++ if/teste.py
@@ -1,8 +1,12 @@
-requested_toppings = []
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni',
+                      'pineapple', 'extra cheese']
 
-if requested_toppings:
-    for requested_topping in requested_toppings:
-        print(f'Adding {requested_topping.title()}')
-    print('\nFinished making your pizza!')
-else:
-    print('Are you sure you want a plain pizza?')
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f'Adding {requested_topping.title()}.')
+    else:
+        print(f'Sorry, we do not have {requested_topping.title()}.')
+
+print('\nFinished making your pizza!')
