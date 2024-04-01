916a927460d9092c00a338dc42ba9c27defaba3c
urassaya <vanessafoxandrade@hotmail.com>
2024-04-01 18:00:09 +0200

    Create favorite_fruit.py

---
+++ if/favorite_fruit.py
@@ -0,0 +1,12 @@
+favorite_fruit = ['grapes', 'passion fruit', 'banana']
+
+if 'grapes' in favorite_fruit:
+    print('You really like grapes!')
+if 'apple' in favorite_fruit:
+    print('You really like apples!')
+if 'passion fruit' in favorite_fruit:
+    print('You really like passion fruit!')
+if 'peach' in favorite_fruit:
+    print('You really like peaches!')
+if 'banana' in favorite_fruit:
+    print('You really like bananas!')
