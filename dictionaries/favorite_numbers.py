b97dc383f80aed2e1bad938bf62f89abb7ea6371
urassaya <vanessafoxandrade@hotmail.com>
2024-04-02 23:58:50 +0200

    Create favorite_numbers.py

---
+++ dictionaries/favorite_numbers.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'rosamund': 1,
+    'léa': 9,
+    'vanessa': 5,
+    'paul': 13,
+    'daniel': 7,
+}
+
+print(f"Rosamund's favorite number is {favorite_numbers['rosamund']}.")
+print(f"Léa's favorite number is {favorite_numbers['léa']}.")
+print(f"Vanessa's favorite number is {favorite_numbers['vanessa']}.")
+print(f"Paul's favorite number is {favorite_numbers['paul']}.")
+print(f"Daniel's favorite number is {favorite_numbers['daniel']}.")
