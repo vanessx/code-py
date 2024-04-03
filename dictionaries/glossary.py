73a2fce1ad8bc71189f6dcff30bbde9a2bfe66ab
urassaya <vanessafoxandrade@hotmail.com>
2024-04-03 21:35:45 +0200

    Create glossary.py

---
+++ dictionaries/glossary.py
@@ -0,0 +1,13 @@
+glossary = {
+    'dictionary': '{}',
+    'lists': '[]',
+    'tuples': '()',
+    'function': 'abc()',
+    'method': 'abc.()',
+    }
+
+print(f'Dictionary:\n\t{glossary['dictionary']}')
+print(f'Lists:\n\t{glossary['lists']}')
+print(f'Tuples:\n\t{glossary['tuples']}')
+print(f'Function:\n\t{glossary['function']}')
+print(f'Method:\n\t{glossary['method']}')
