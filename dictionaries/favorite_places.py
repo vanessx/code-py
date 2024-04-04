8b187c72f44014fc9e253717c19a396829536481
urassaya <vanessafoxandrade@hotmail.com>
2024-04-04 22:16:48 +0200

    Create favorite_places.py

---
+++ dictionaries/favorite_places.py
@@ -0,0 +1,10 @@
+favorite_places = {
+    'nathalie': ['porto', 'rome', 'paris'],
+    'margot': ['madeira', 'toscana', 'stockholm'],
+    'rosamund': ['shanghai', 'bangkok', 'rio de janeiro'],
+}
+
+for person, places in favorite_places.items():
+    print(f'\n{person.title()} favorite places are:')
+    for place in places:
+        print(f'\t- {place.title()}')
