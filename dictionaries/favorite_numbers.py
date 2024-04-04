fc5d4ad7b326ee42099e98bf48f37bc249bf3d3d
urassaya <vanessafoxandrade@hotmail.com>
2024-04-04 22:23:46 +0200

    update

--- dictionaries/favorite_numbers.py
+++ dictionaries/favorite_numbers.py
@@ -1,13 +1,18 @@
 favorite_numbers = {
-    'rosamund': 1,
-    'léa': 9,
-    'vanessa': 5,
-    'paul': 13,
-    'daniel': 7,
-}
+    'rosamund': [1, 3],
+    'léa': [7, 2, 10],
+    'vanessa': [5, 1, 9],
+    'paul': [13, 26],
+    'daniel': [6],
+    }
 
-print(f"Rosamund's favorite number is {favorite_numbers['rosamund']}.")
-print(f"Léa's favorite number is {favorite_numbers['léa']}.")
-print(f"Vanessa's favorite number is {favorite_numbers['vanessa']}.")
-print(f"Paul's favorite number is {favorite_numbers['paul']}.")
-print(f"Daniel's favorite number is {favorite_numbers['daniel']}.")
+for person, numbers in favorite_numbers.items():
+    print(f"\n{person.title()}'s favorite numbers are:", end=" ")
+    for number in numbers:
+        print(f'{number}', end=" ")
+
+#print(f"Rosamund's favorite number is {favorite_numbers['rosamund']}.")
+#print(f"Léa's favorite number is {favorite_numbers['léa']}.")
+#print(f"Vanessa's favorite number is {favorite_numbers['vanessa']}.")
+#print(f"Paul's favorite number is {favorite_numbers['paul']}.")
+#print(f"Daniel's favorite number is {favorite_numbers['daniel']}.")
--- dictionaries/favorite_places.py
+++ dictionaries/favorite_places.py
@@ -3,9 +3,9 @@ favorite_places = {
     'nathalie': ['porto', 'rome', 'paris'],
     'margot': ['madeira', 'toscana', 'stockholm'],
     'rosamund': ['shanghai', 'bangkok', 'rio de janeiro'],
-}
+    }
 
 for person, places in favorite_places.items():
-    print(f'\n{person.title()} favorite places are:')
+    print(f"\n{person.title()}'s favorite places are:")
     for place in places:
         print(f'\t- {place.title()}')
