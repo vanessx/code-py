7629a462a5e8268f91a02a02bb7aed0f3421fde6
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-07 20:08:24 +0200

    commit

---
+++ function/large_shirts.py
@@ -0,0 +1,6 @@
+def make_shirt(size='L', text='I love Python'):
+    print(f'The size of the tshirt is {size} and the text is: {text}.')
+
+make_shirt()
+make_shirt('M')
+make_shirt(text='Letterboxd')
---
+++ function/tshirt.py
@@ -0,0 +1,5 @@
+def make_shirt(size, text):
+    print(f'The size of the tshirt is {size} and the text is: {text}.')
+
+make_shirt('M', 'I love Python!')
+make_shirt(size='L', text='Code? Hm...')
