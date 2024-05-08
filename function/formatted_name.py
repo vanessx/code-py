526e5e63d7de197baebab829eb69cc7ac266b56b
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-08 15:50:36 +0200

    Update formatted_name.py

--- function/formatted_name.py
+++ function/formatted_name.py
@@ -1,7 +1,14 @@
 # return a simple value
-def formatted_name(first_name, last_name):
-    full_name = f'{first_name} {last_name}'
+# middle name como opcional, por isso atribuimos um default value
+def formatted_name(first_name, last_name, middle_name=''):
+    if middle_name:
+        full_name = f'{first_name} {middle_name} {last_name}'
+    else:
+        full_name = f'{first_name} {last_name}'
     return full_name.title()
 
 musician = formatted_name('jimi', 'hendrix')
+print(musician)
+
+musician = formatted_name('john', 'hooker', 'lee')
 print(musician)
