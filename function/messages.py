e8591681bcc149209300d93aaa72730bf83047a2
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-10 16:04:30 +0200

    commit

--- function/messages.py
+++ function/messages.py
@@ -1,8 +1,8 @@
-def messages(texts):
+def show_messages(texts):
     for text in texts:
         print(text)
 
 msg = ['i love movies', 'do you want pizza or lasagna?',
             'cats are the best', 'python?']
 
-messages(msg)
+show_messages(msg)
---
+++ function/sending_messages.py
@@ -0,0 +1,20 @@
+def show_messages(texts):
+    for text in texts:
+        print(text)
+
+def send_messages(texts, sent_texts):
+    while texts:
+        current_text = texts.pop()
+        print(f'Sending message: {current_text}.')
+        sent_texts.append(current_text)
+
+msg = ['i love movies', 'do you want pizza or lasagna?', 
+       'cats are the best', 'python?']
+show_messages(msg)
+
+sent_msg = []
+send_messages(msg, sent_msg)
+
+print('\nFinal list:')
+print(msg)
+print(sent_msg)
