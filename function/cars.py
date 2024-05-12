184e008a6fb2e91d28ceedb0b54ac30a276b5959
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-12 23:28:37 +0200

    Update cars.py

--- function/cars.py
+++ function/cars.py
@@ -1,7 +1,15 @@
 def description_car(brand, model, **car_info):
-    car_info['brand'] = brand
-    car_info['model'] = model
-    print(car_info)
+    car_dic = {
+        'brand': brand.title(),
+        'model': model.title(),
+    }
+    for car, value in car_info.items():
+        car_dic[car] = value
+        
+    return car_dic
 
-description_car('bmw', 'x6')
-description_car('audi', 'r8', fuel_type='electric', speed=210)
+my_car = description_car('bmw', 'x6')
+print(my_car)
+
+your_car = description_car('audi', 'r8', fuel_type='electric', speed=210)
+print(your_car)
