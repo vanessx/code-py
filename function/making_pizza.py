59ea9f186e16caf0bab4b5e8aef8909a5b6b1470
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-13 21:34:37 +0200

    commit

---
+++ function/making_pizza.py
@@ -0,0 +1,5 @@
+from pizza import make_pizza as mp
+# quando o nome da função é longo ou entra em conflito com o resto do código->
+# podemos usar um 'alias' que é dar um nickname como vemos em 'as mp'
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
--- function/pizza.py
+++ function/pizza.py
@@ -3,6 +3,3 @@ def make_pizza(size, *toppings):
     print(f'Making a {size}-inch pizza with the following toppings:')
     for topping in toppings:
         print(f'- {topping}')
-
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
