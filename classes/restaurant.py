f3af1018fab899ee63cf208f429cd8df6dc837e4
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-14 22:46:13 +0200

    Update restaurant.py

--- classes/restaurant.py
+++ classes/restaurant.py
@@ -4,14 +4,20 @@ class Restaurant:
         self.cuisine = cuisine_type
     
     def describe_restaurant(self):
-        print(f"The restaurant's name is {self.name}.\n"
+        print(f"\nThe restaurant's name is {self.name.title()}.\n"
               f"The cuisine's type is {self.cuisine}.")
 
     def open_restaurant(self):
-        print(f'The {self.name.title()} is open. Come on in!')
+        print(f'The {self.name} is open. Come on in!')
 
 restaurant = Restaurant('aero', 'seafood')
 print(f'{restaurant.name.title()}. {restaurant.cuisine}.')
 
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+
+north_restaurant = Restaurant('plate of luck', 'classique')
+north_restaurant.describe_restaurant()
+
+south_restaurant = Restaurant('fornier', 'française')
+south_restaurant.describe_restaurant()
