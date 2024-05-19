d1b1bcf245276aa51a1cad1fc17633407bcbcc58
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-20 00:39:25 +0200

    Update car.py

--- classes/car.py
+++ classes/car.py
@@ -3,10 +3,28 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
     
     def describe_car(self):
         long_name = f'{self.year} {self.make} {self.model}'
         return long_name.title()
     
+    def read_odometer(self):
+        print(f'This car has {self.odometer_reading} miles on it.')
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.describe_car())
+
+# mudar o valor de um atributo diretamente
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
+# mudar o valor de um atributo através de um método (função)
+my_new_car.update_odometer(24)
+my_new_car.read_odometer()
