005614ef0803721da933b63fedb54f0fb1441cb1
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-27 22:34:14 +0200

    commit

--- classes/admin.py
+++ classes/admin.py
@@ -1,25 +1,4 @@
-class User:
-    def __init__(self, first_name, last_name, age, gender, location):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-        self.gender = gender
-        self.location = location
-        self.login_attempts = 0
-    
-    def describe_user(self):
-        print(f'Your name is {self.first_name.title()} {self.last_name.title()}'
-              f' and you are {self.age} years old.')
-        print(f'Your gender is {self.gender} and you live in {self.location.title()}.')
-
-    def greet_user(self):
-        print(f'Hello, {self.first_name.title()} {self.last_name.title()}!')
-
-    def increment_login_attempts(self):
-        self.login_attempts += 1
-    
-    def reset_login_attempts(self):
-        self.login_attempts = 0
+from user import User
 
 class Privileges:
     def __init__(self):
--- classes/my_admin.py
+++ classes/my_admin.py
@@ -1,5 +1,5 @@
-import admin
+from admin import Admin
 
-my_admin = admin.Admin('léa', 'seydoux', 38, 'female', 'paris')
+my_admin = Admin('léa', 'seydoux', 38, 'female', 'paris')
 my_admin.describe_user()
 my_admin.privileges.show_privileges()
