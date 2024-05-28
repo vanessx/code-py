646898d381f7a1a93a425348bb748ef96b4045d6
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-28 14:16:47 +0200

    Update file_reader.py

--- filesandexceptions/file_reader.py
+++ filesandexceptions/file_reader.py
@@ -1,3 +1,18 @@
+# abrir um ficheiro que está na mesma pasta
 with open('pi_digits.txt') as file_object:
     contents = file_object.read()
 print(contents)
+
+# abrir um ficheiro que está em outra pasta (relative file path)
+"""
+with open('foldername/filename.txt') as file_object:
+
+"""
+
+# abrir um ficheiro que está em outra pasta (absolute file path)
+# como são longos é mais fácil atribuir o file path a uma variável
+"""
+file_path = '/home/vanessx/other_files/text_files/filename.txt'
+with open(file_path) as file_object:
+
+"""
