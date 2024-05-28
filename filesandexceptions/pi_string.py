eab34b207a03ccc920c815297b4447d7a4bfea7f
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-28 15:14:05 +0200

    Update pi_string.py

--- filesandexceptions/pi_string.py
+++ filesandexceptions/pi_string.py
@@ -2,7 +2,7 @@
 filename = 'text_files/pi_digits.txt'
 
 with open(filename) as file_object:
-    # o método readlines( pega cada linha e guarda numa lista (neste caso: files))
+    # o método readlines() pega cada linha e guarda numa lista (neste caso: files))
     lines = file_object.readlines()
 
 pi_string = ''
