6406d20c553471109fab96c3b3cf81014cdcb4bc
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-28 14:30:36 +0200

    commit

--- filesandexceptions/file_reader.py
+++ filesandexceptions/file_reader.py
@@ -1,4 +1,4 @@
-# abrir um ficheiro que está em outra pasta (relative file path)
+# abrir e ler um ficheiro que está em outra pasta (relative file path)
 with open('text_files/pi_digits.txt') as file_object:
     contents = file_object.read()
 print(contents)
@@ -14,3 +14,10 @@ with open('filename.txt') as file_object:
 file_path = '/home/vanessx/other_files/text_files/filename.txt'
 with open(file_path) as file_object:
 """
+
+# ler linha por linha
+filename = 'text_files/names.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
