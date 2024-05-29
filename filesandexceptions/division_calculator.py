8233eb9066c1ccaa00779154e7d7e90abe546027
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-29 20:31:27 +0200

    Update division_calculator.py

--- filesandexceptions/division_calculator.py
+++ filesandexceptions/division_calculator.py
@@ -1,4 +1,16 @@
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("You can't divide by zero!")
+print('Give me two numbers, and I will divide them.\n'
+      'Enter \033[031mq\033[m to quit.')
+
+while True:
+    first_num = input('First number:\n')
+    if first_num == 'q':
+        break
+    second_num = input('Second number:\n')
+    if second_num == 'q':
+        break
+    try:
+        answer = int(first_num) / int(second_num)
+    except ZeroDivisionError:
+        print("You can't divide by 0!")
+    else:
+        print(answer)
