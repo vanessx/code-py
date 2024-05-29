1c7ffda6c9f5b7cdff35f14c26c0b860c214babb
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-29 22:29:38 +0200

    Update addition.py

--- filesandexceptions/addition.py
+++ filesandexceptions/addition.py
@@ -1,9 +1,19 @@
-try:
-    first_num = int(input('First number:\n'))
-    second_num = int(input('Second number:\n'))
-except ValueError:
-    # caso o user introduza letras
-    print('Please, enter numbers.')
-else:
-    result = first_num + second_num
-    print(f'The sum of {first_num} and {second_num} is \033[031m{result}\033[m.')
+print('Press \033[031mq\033[m to quit.')
+while True:
+    try:
+        first_num = input('First number:\n')
+        if first_num == 'q':
+            break
+        first_num = int(first_num)
+
+        second_num = input('Second number:\n')
+        if second_num == 'q':
+            break
+        second_num = int(second_num)
+
+    except ValueError:
+        # caso o user introduza letras
+        print('Please, enter numbers.')
+    else:
+        result = first_num + second_num
+        print(f'The sum of {first_num} and {second_num} is \033[031m{result}\033[m.')
