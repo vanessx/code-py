7da2be07d2ef19fb31f09ae7d87c6f6ab02e1f31
vanessa <51798590+vanessx@users.noreply.github.com>
2024-05-31 13:12:23 +0200

    Update remember_me.py

--- filesandexceptions/remember_me.py
+++ filesandexceptions/remember_me.py
@@ -1,14 +1,31 @@
 import json
-# lemos o username caso já tenha sido fornecido
-# caso contrário, perguntamos o username e guardamos
-filename = 'username.json'
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
+
+def get_stored_username():
+    """vai buscar o username se possível"""
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def new_username():
+    """prompt para um user novo"""
     username = input("What's your name?\n")
+    filename = 'username.json'
     with open(filename, 'w') as f:
         json.dump(username, f)
+    return username
+
+def greet_user():
+    """dá as boas vindas ao user pelo username"""
+    username = get_stored_username()
+    if username:
+        print(f'Welcome back, {username}!')
+    else:
+        username = new_username()
         print(f"We'll remember you when you come back, {username}!")
-else:
-    print(f'Welcome back, {username}!')
+
+greet_user()
