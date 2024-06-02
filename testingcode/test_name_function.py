0e582d58b74ee8e814e44ed59a732903baba2bb5
vanessa <51798590+vanessx@users.noreply.github.com>
2024-06-02 13:18:58 +0200

    commit

--- testingcode/test_name_function.py
+++ testingcode/test_name_function.py
@@ -6,6 +6,10 @@ class NamesTestCase(unittest.TestCase):
     def test_first_last_name(self):
         full_name = get_formatted_name('margot', 'robbie')
         self.assertEqual(full_name, 'Margot Robbie')
+    
+    def test_first_last_middle_name(self):
+        full_name = get_formatted_name('anya', 'joy', 'taylor')
+        self.assertEqual(full_name, 'Anya Taylor Joy')
 
 if __name__ == '__main__':
     unittest.main()
