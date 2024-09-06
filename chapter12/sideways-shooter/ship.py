c520c421d0c8cba6cbf17c657a81828c57d148a4
vanessa <51798590+vanessx@users.noreply.github.com>
2024-09-06 16:39:27 +0200

    commit

--- chapter12/sideways-shooter/ship.py
+++ chapter12/sideways-shooter/ship.py
@@ -3,4 +3,18 @@ import pygame
 
 class Ship:
 
-    def __init__(self):
+    def __init__(self, ss_game):
+        # start the ship and set its starting position
+        self.screen = ss_game.screen
+        self.settings = ss_game.settings
+        self.screen_rect = ss_game.screen.get_rect()
+
+        # loading the ship and get its rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # start each new ship at the left center of the screen
+        self.rect.midleft = self.screen_rect.midleft
+
+        # store a decimal value for the ship's vertical position
+        self.y = float(self.rect.y)
